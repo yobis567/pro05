39050172b074a4b897175f3bec7d3e0805b6f6b9
yobis567 <yobis567@naver.com>
2019-10-02 12:28:52 +0900

    11

---
+++ pro05/src/sec01/ex01/FirstServlet.java
@@ -0,0 +1,30 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+
+public class FirstServlet extends HttpServlet {
+       
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("destory 메서드 호출");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet 메서드 호출");
+	}
+
+}
